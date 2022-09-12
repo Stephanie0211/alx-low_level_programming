780b72668648d6aee2c502274eef3ef17675d2ad
stephanie <stephanieifeoma11>
2022-09-12 16:21:14 -0700

    programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
-
+/**
+ * main or otherwise called the entry point is used to show the executable part of the programe
+ */
 int main(void)
 {
-	puts(" \"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
